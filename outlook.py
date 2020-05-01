56d06c94a4155a8029fd14b80961d9e109d723b2
ldaxr <ldaxr@outlook.com>
2020-05-01 22:55:05 +0800

    2222

--- outlook.py
+++ outlook.py
@@ -13,10 +13,10 @@ def GrabURL(url, filename, x1, y1, x2, y2) :
     im = ImageGrab.grab((x1, y1, x2, y2)) 
     im.save(filename,'JPEG') 
     
-GrabURL('http://www.baidu.com', 'baidu.jpg', 100, 100, 1000, 1000)
-GrabURL('http://www.bing.com', 'bing.jpg', 100, 100, 1000, 1000)
+GrabURL('http://www.baidu.com', 'file1.jpg', 100, 100, 1000, 1000)
+GrabURL('http://www.bing.com', 'file2.jpg', 100, 100, 1000, 1000)
 
-
+cwd = os.getcwd()
 
 outlook = win32.Dispatch('Outlook.Application')
 Mail_Item = outlook.CreateItem(0)
@@ -24,16 +24,15 @@ Mail_Item.Recipients.Add('12345@qq.com')
 
 Mail_Item.Subject ='test mail with images'
 Mail_Item.BodyFormat = 2
-Mail_Item.Attachments.Add(r'C:\Users\1325\Documents\baidu.jpg')
-Mail_Item.Attachments.Add(r'C:\Users\1325\Documents\bing.jpg')
-Mail_Item.Attachments.Add(r'C:\Users\1325\Documents\test.py')
+Mail_Item.Attachments.Add(cwd + r'\file1.jpg')
+Mail_Item.Attachments.Add(cwd + r'\file2.jpg')
 Mail_Item.HtmlBody = '''
 <html><body>
 <a href=http://www.baidu.com>Baidu</a><br>
-<img src=baidu.jpg /><br>
+<img src=file1.jpg /><br>
 <a href=http://www.bing.com>Bing</a><br>
-<img src=bing.jpg /><br>
+<img src=file2.jpg /><br>
 </body></html>
 '''
 Mail_Item.Display()
-Mail_Item.Send()
+#Mail_Item.Send()
